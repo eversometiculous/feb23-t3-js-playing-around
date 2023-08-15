a1c257f73e291caeff64325d18254e6217baba01
eversometiculous <126760003+eversometiculous@users.noreply.github.com>
2023-08-15 20:54:35 +1000

    added index.html and script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+
+/**
+* This is a function
+* @param {string} someArgument - Some data to give to the function
+*/
+
+function someFunction(someArgument) {
+    console.log("This was given to the function:" + some Argument);
+}
+
+someFunction([1, 2, "banana"]);
